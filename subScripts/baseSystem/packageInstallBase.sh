1c35c8383a7e2b979001532b808a8479ef004ca4
SamuelMcPherson <contact@samuelmcpherson.net>
2021-10-11 20:58:24 -0700

    Added dropbear ssh access to zfsbootmenu

--- subScripts/baseSystem/packageInstallBase.sh
+++ subScripts/baseSystem/packageInstallBase.sh
@@ -14,10 +14,10 @@ fi
 
 if [ -n "$BIOS" ] 
 then
-    chroot $TEMPMOUNT /bin/bash -c "apt -y install refind && echo '---> apt install refind succeeded <--------------------------------------------------------------' || apt -y install refind  failed'; exit 1; }" || exit 1
+    chroot $TEMPMOUNT /bin/bash -c "apt -y install refind dracut-network dropbear && echo '---> apt install refind dracut-network dropbear succeeded <--------------------------------------------------------------' || apt -y install refind dracut-network dropbear failed'; exit 1; }" || exit 1
 elif [ -n "$EFI" ]
 then
-    chroot $TEMPMOUNT /bin/bash -c "apt -y install refind efibootmgr && echo '---> apt install refind efibootmgr succeeded <--------------------------------------------------------------' || { echo 'apt install refind efibootmgr failed'; exit 1; }" || exit 1
+    chroot $TEMPMOUNT /bin/bash -c "apt -y install refind efibootmgr dracut-network dropbear && echo '---> apt install refind efibootmgr dracut-network dropbear succeeded <--------------------------------------------------------------' || { echo 'apt install refind efibootmgr dracut-network dropbear failed'; exit 1; }" || exit 1
 fi
 
 chroot $TEMPMOUNT /bin/bash -c "apt -y install locales vim git rsync dosfstools openssh-server curl patch apt-file software-properties-common apt-transport-https && echo '---> apt install locales vim git rsync dosfstools openssh-server curl patch apt-file software-properties-common apt-transport-https succeeded <--------------------------------------------------------------' || { echo 'apt install locales vim git rsync dosfstools openssh-server curl patch apt-file software-properties-common apt-transport-https failed'; exit 1; }" || exit 1
