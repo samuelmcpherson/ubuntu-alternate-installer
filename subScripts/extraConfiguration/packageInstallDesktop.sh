3821fe2ac4f4363af160a55a3ace6f49f8a446db
SamuelMcPherson <contact@samuelmcpherson.net>
2021-10-12 20:48:37 -0700

    Fixed external desktop apps issues, slack deb still needs work

--- subScripts/extraConfiguration/packageInstallDesktop.sh
+++ subScripts/extraConfiguration/packageInstallDesktop.sh
@@ -25,15 +25,15 @@ chroot $TEMPMOUNT /bin/bash -c "echo 'deb https://paulcarroty.gitlab.io/vscodium
 
 chroot $TEMPMOUNT /bin/bash -c "wget -qO - https://protonmail.com/download/bridge_pubkey.gpg | gpg --dearmor | sudo dd of=/etc/apt/trusted.gpg.d/bridge_pubkey.gpg"
 
-chroot $TEMPMOUNT /bin/bash -c "cd /tmp && wget -O protonmail-bridge.deb 'https://protonmail.com/download/protonmail-bridge_1.8.9-1_amd64.deb'"
+chroot $TEMPMOUNT /bin/bash -c "cd /tmp && wget -O protonmail-bridge.deb 'https://protonmail.com/download/bridge/protonmail-bridge_1.8.9-1_amd64.deb'"
 
 
 
-chroot $TEMPMOUNT /bin/bash -c "cd /tmp && wget -O discord.deb 'https://discordapp.com/api/download?platform=linux&format=deb'"
+#chroot $TEMPMOUNT /bin/bash -c "cd /tmp && wget -O discord.deb 'https://discordapp.com/api/download?platform=linux&format=deb'"
 
 
 
-chroot $TEMPMOUNT /bin/bash -c "cd /tmp && wget -O slack-desktop.deb https://downloads.slack-edge.com/linux_releases/slack-desktop-4.13.0-amd64.deb"
+#chroot $TEMPMOUNT /bin/bash -c "cd /tmp && wget -O slack-desktop.deb https://downloads.slack-edge.com/linux_releases/slack-desktop-4.13.0-amd64.deb"
 
 
 
@@ -41,7 +41,7 @@ chroot $TEMPMOUNT /bin/bash -c "cd /tmp && wget https://zoom.us/client/latest/zo
 
 
 
-chroot $TEMPMOUNT /bin/bash -c "cd /tmp && wget https://github.com/jgraph/drawio-desktop/releases/download/v15.4.0/draw.io-amd64-15.4.0.deb"
+chroot $TEMPMOUNT /bin/bash -c "cd /tmp && wget https://github.com/jgraph/drawio-desktop/releases/download/v15.4.0/drawio-amd64-15.4.0.deb"
 
 chroot $TEMPMOUNT /bin/bash -c "cd /tmp && wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
 
@@ -59,9 +59,9 @@ chroot $TEMPMOUNT /bin/bash -c "apt install -y codium && echo '---> apt install 
 
 chroot $TEMPMOUNT /bin/bash -c "apt install -y /tmp/protonmail-bridge.deb && echo '---> apt install /tmp/protonmail-bridge.deb succeeded <--------------------------------------------------------------' || echo '---> apt install /tmp/protonmail-bridge.deb failed <--------------------------------------------------------------'"
 
-chroot $TEMPMOUNT /bin/bash -c "apt install -y /tmp/discord.deb && echo '---> apt install /tmp/discord.deb succeeded <--------------------------------------------------------------' || echo '---> apt install /tmp/discord.deb failed <--------------------------------------------------------------'"
+#chroot $TEMPMOUNT /bin/bash -c "apt install -y /tmp/discord.deb && echo '---> apt install /tmp/discord.deb succeeded <--------------------------------------------------------------' || echo '---> apt install /tmp/discord.deb failed <--------------------------------------------------------------'"
 
-chroot $TEMPMOUNT /bin/bash -c "apt install -y /tmp/slack-desktop.deb && echo '---> apt install /tmp/slack-desktop.deb succeeded <--------------------------------------------------------------' || echo '---> apt install /tmp/slack-desktop.deb failed <--------------------------------------------------------------'"
+#chroot $TEMPMOUNT /bin/bash -c "apt install -y /tmp/slack-desktop.deb && echo '---> apt install /tmp/slack-desktop.deb succeeded <--------------------------------------------------------------' || echo '---> apt install /tmp/slack-desktop.deb failed <--------------------------------------------------------------'"
 
 chroot $TEMPMOUNT /bin/bash -c "apt install -y /tmp/zoom_amd64.deb && echo '---> apt install zoom_amd64.deb succeeded <--------------------------------------------------------------' || echo '---> apt install zoom_amd64.deb failed <--------------------------------------------------------------'"
 
